09273105a262a818f78f3c9788c63c64efeb1251
ahmed adel <official3adool@gmail.com>
2026-05-09 12:58:45 +0300

    test: calculating days

--- src/test/java/stopwatch/StopWatchTest.java
+++ src/test/java/stopwatch/StopWatchTest.java
@@ -31,7 +31,7 @@ public class StopWatchTest {
         Assertions.assertEquals(2,hours,"stopWatch does have hours");
 
     }
-    //test comulative values of the minutes
+    //test cumulative values of the minutes
     @Test
     void test4(){
         StopWatch stopWatch = new StopWatch();
@@ -40,6 +40,25 @@ public class StopWatchTest {
         stopWatch.record(30);
         stopWatch.record(30);
         Assertions.assertEquals(2,stopWatch.getHours(),"stop watch doesn't acumulate minutes");
+    }
+    // test days
+    @Test
+    void test5(){
+        StopWatch stopWatch = new StopWatch(8);
+        stopWatch.record(60);
+        stopWatch.record(60);
+        stopWatch.record(60);
+        stopWatch.record(60);
+        stopWatch.record(60);
+        stopWatch.record(60);
+        stopWatch.record(60);
+        stopWatch.record(60);
+        //1 day
+        stopWatch.record(60);
+        //1 hour
+        stopWatch.record(30);
+        //30 mins
+        Assertions.assertEquals(1,stopWatch.getDays(),"stop watch doesn't calculate days");
 
     }
 
