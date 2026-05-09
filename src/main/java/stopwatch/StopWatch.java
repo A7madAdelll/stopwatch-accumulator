7a52b08b98b69294110be5c68842cf3c24a05497
ahmed adel <official3adool@gmail.com>
2026-05-09 12:02:00 +0300

    stopwatch now has hours

--- src/main/java/stopwatch/StopWatch.java
+++ src/main/java/stopwatch/StopWatch.java
@@ -5,8 +5,11 @@ public class StopWatch {
     int hours;
     public void record(int minutes) {
         if (minutes >= 0) {
-            this.minutes = minutes;
+            this.minutes = minutes; //this supposed to be comulative
+            this.hours+=minutes/60;
+            this.minutes=minutes%60;
         }
+
     }
 
     public int getMinutes() {
